da85164c11baed5eb786a2c93d3f2aa490ae10df
Aaron Smith <asmith123@elon.edu>
2023-04-24 10:48:38 -0400

    code cleanup

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.model.AppUser;
+import com.example.demo.model.Question;
 import com.example.demo.repository.AppUserRepository;
 import com.example.demo.repository.QuestionRepository;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.Instant;
+import java.util.Arrays;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -23,7 +27,21 @@ public class DemoApplication {
 		AppUser user = new AppUser();
 		user.setUsername("admin");
 		user.setPassword("admin");
-		return args -> appUserRepository.save(user);
+		appUserRepository.save(user);
+		// create initial questions
+		return args -> {
+			Question question1 = new Question();
+			question1.setQuestionDescription("Do you like Java?");
+			question1.setAppUser(user);
+			question1.setCreatedTimestamp(Instant.now());
+
+			Question question2 = new Question();
+			question2.setQuestionDescription("Do you like Angular?");
+			question2.setAppUser(user);
+			question2.setCreatedTimestamp(Instant.now());
+
+			questionRepository.saveAll(Arrays.asList(question1, question2));
+		};
 	}
 
 }
